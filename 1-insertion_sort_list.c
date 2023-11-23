14baccc8b1616ba1219c6f5199397bd4b5de51ab
RuthWanzila <wanzilaruth533@gmail.com>
2023-11-23 14:04:22 -0800

    sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
-* insertion_sort_list - Sorts a doubly linked list of
-* integers in ascending order using Insertion sort
+* insertion_sort_list - Sorts a doubly linked list of integers
+* in ascending order using Insertion sort
 * @list: Pointer to the head of the doubly linked list
 */
 void insertion_sort_list(listint_t **list)
